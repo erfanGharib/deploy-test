03530cdf6723e2e54aa09a9c39915e3af1ca5939
erfan gharib <erfangharib5@gmail.com>
2023-08-03 19:16:14 +0430

    add liara.yaml

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import styles from '@/styles/Home.module.css';
+// import styles from '@/styles/Home.module.css';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main>
         <h1>Hello World</h1>
       </main>
     </>
